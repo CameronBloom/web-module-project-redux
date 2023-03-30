755f91131ff961233c0e99d07e069dd7d1aee853
Cameron Morgan <cameronmorgan@Camerons-MacBook-Air.local>
2023-03-30 15:52:19 -0400

    Connected the 'Movie' and 'MovieHeader' components.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => {
+    return ({
+        movies: state.movies
+    })
+}
+
 const Movie = (props) => {
+    const { movies } = props;
     const { id } = useParams();
     const { push } = useHistory();
 
-    const movies = [];
     const movie = movies.find(movie=>movie.id===Number(id));
     
     return(<div className="modal-page col">
@@ -47,4 +55,4 @@ const Movie = (props) => {
     </div>);
 }
 
-export default Movie;
+export default connect(mapStateToProps)(Movie);
--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state) => {
+    return ({
+        appTitle: state.appTitle
+    })
+}
+
 const MovieHeader = (props) => {
-    const appTitle = "";
+    const { appTitle } = props;
     const displayFavorites = true;
     
     return(<div className="table-title">
@@ -19,4 +27,4 @@ const MovieHeader = (props) => {
     </div>);
 }
 
-export default MovieHeader;
+export default connect(mapStateToProps)(MovieHeader);
